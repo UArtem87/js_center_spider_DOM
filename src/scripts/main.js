0e2d7175954555880abaca98b06f3dd184ed42f2
Artem <usik887@ukr.net>
2026-02-22 01:03:26 +0200

    added condition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,33 @@
 'use strict';
 
+'use strict';
+
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.style.position = 'relative';
-spider.style.position = 'absolute';
-
-if (spider.parentElement !== wall) {
-  wall.append(spider);
+if (!wall || !spider) {
+  throw new Error('Required DOM elements (.wall or .spider) are missing.');
+} else {
+  initSpider();
 }
 
-const centerSpider = () => {
-  const x = (wall.clientWidth - spider.offsetWidth) / 2;
-  const y = (wall.clientHeight - spider.offsetHeight) / 2;
+function initSpider() {
+  wall.style.position = 'relative';
+  spider.style.position = 'absolute';
 
-  spider.style.left = `${x}px`;
-  spider.style.top = `${y}px`;
-};
+  const centerSpider = () => {
+    const x = (wall.clientWidth - spider.offsetWidth) / 2;
+    const y = (wall.clientHeight - spider.offsetHeight) / 2;
 
-centerSpider();
+    spider.style.left = `${x}px`;
+    spider.style.top = `${y}px`;
+  };
 
-window.addEventListener('resize', centerSpider);
+  if (spider.complete) {
+    centerSpider();
+  } else {
+    spider.addEventListener('load', centerSpider);
+  }
+
+  window.addEventListener('resize', centerSpider);
+}
