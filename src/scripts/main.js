1c66110e2f475b54fd6e2e010b0e9340ef324068
Artem <usik887@ukr.net>
2026-02-22 00:38:27 +0200

    update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,19 @@ const spider = document.querySelector('.spider');
 
 wall.style.position = 'relative';
 spider.style.position = 'absolute';
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
 
-wall.append(spider);
+if (spider.parentElement !== wall) {
+  wall.append(spider);
+}
+
+const centerSpider = () => {
+  const x = (wall.clientWidth - spider.offsetWidth) / 2;
+  const y = (wall.clientHeight - spider.offsetHeight) / 2;
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
+};
+
+centerSpider();
+
+window.addEventListener('resize', centerSpider);
